87abd7f0c85d67638e1c7f87afbfc946c6fc212c
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-07-14 11:57:56 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,10 @@
 from sdg.open_sdg import open_sdg_build
 
+def alter_meta(meta):
+  if data_footnote in meta:
+    footnote = meta['data_footnote']
+    footnote_parts=
+    new_footnote="<br><ol style='padding-left: 9rem;'><li>"+data_footnote.replace(";","</li><li>")+"</li></ol>"
+    
+
 open_sdg_build(config='config_data.yml')
